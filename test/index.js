bfcc36e87316038f52850d855a374b7ef0f2fcae
Jacobo <jacobo@xarope.eu>
2016-04-25 18:26:10 +0200

    add test index

---
+++ test/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+require('./is-hex-color-test.js')
+require('./get-color-code-test.js')
+require('./get-ui-test.js')
+require('./get-colors-test.js')
+require('./parse-style-string-test.js')
+require('./get-schemas-test.js')
