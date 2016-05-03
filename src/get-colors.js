f4a9c30e9493b1acdf232d737b2bad1d8557230d
Jacobo Tabernero <jacobo@xarope.eu>
2016-05-03 19:41:25 +0200

    Update: fix missing comment highlight

--- src/get-colors.js
+++ src/get-colors.js
@@ -2,14 +2,14 @@
 
 const isHexColor = require('./is-hex-color.js')
 
-// get an associative array with color list for the template
+// get a new object with valid color codes
 module.exports = function (data) {
   const colors = {}
   if (data && typeof data === 'object') {
     Object.keys(data).forEach(name => {
       let code = data[name]
       if (!code || !isHexColor(code)) {
-        throw new Error('color ' + name + ' is invalid')
+        throw new Error(`color ${ name } is invalid`)
       }
       colors[name] = code
     })
