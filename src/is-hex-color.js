4534037f3a5758aaa003843e2899652e4e086328
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-07 21:11:34 +0200

    Update: is-hex-color in typescript

--- src/is-hex-color.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = function (color) {
-  return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color)
-}
--- src/is-hex-color.ts
+++ src/is-hex-color.ts
@@ -1,5 +1,3 @@
-'use strict'
-
 export default function (color: string): boolean {
   return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color)
 }
