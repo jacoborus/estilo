2a2d4732298801efba605be6a3a8b4ab7ac921a3
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-07 21:16:06 +0200

    Fix: Missing changes in compile-palette typescript

--- src/compile-palette.ts
+++ src/compile-palette.ts
@@ -1,23 +1,34 @@
-'use strict'
+import path from 'path'
+import hexterm from 'hexterm'
+import { Palette, YmlFile } from './common'
+import isHexColor from './is-hex-color'
 
-const isHexColor = require('./is-hex-color.js')
-const hexterm = require('hexterm')
+export default function (file: YmlFile): Palette {
+  const { content, filepath } = file
+  if (typeof content !== 'object') {
+    throw new Error(`Content of palette (${filepath}) is not an object`)
+  }
 
-module.exports = function (colors, paletteName) {
-  const palette = {}
-  Object.keys(colors).forEach(c => {
-    const str = colors[c].trim()
+  const palette = {
+    name: path.basename(filepath, '.yml'),
+    path: filepath,
+    colors: {}
+  } as Palette
 
-    const props = str.split(' ').reduce((arr, el) => {
-      el = el.trim()
-      if (el) return arr.concat(el)
-      return arr
-    }, [])
+  Object.keys(content).forEach(name => {
+    const hexcolor = content[name].trim()
 
-    if (!props[0] || !isHexColor(props[0])) {
-      throw new Error(`color ${c} is invalid. Palette: ` + paletteName)
+    if (typeof hexcolor !== 'string') {
+      throw new Error(`Wrong type: ${filepath}: ${name}`)
     }
-    palette[c] = [props[0], props[1] || hexterm(props[0])]
+    if (!isHexColor(hexcolor)) {
+      throw new Error(`Wrong color: ${filepath}: ${name}`)
+    }
+
+    palette.colors[name] = {
+      hex: hexcolor.startsWith('#') ? hexcolor : '#' + hexcolor,
+      xterm: hexterm(hexcolor)
+    }
   })
   return palette
 }
