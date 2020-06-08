87c9acd5146b203840d8d21ac3a980570babf602
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-08 03:15:12 +0200

    Update: Begin load project with typescript

---
+++ src/load-project.ts
@@ -0,0 +1,60 @@
+import fs from 'fs'
+import path from 'path'
+import { loadYml } from './load-yml'
+import { loadPalette } from './load-palette'
+import {
+  Palettes,
+  Project,
+  SyntaxFile,
+  StatusStyle
+} from './common'
+
+export function createProject (folderPath: string): Project {
+  return {
+    config: loadYml(folderPath, 'estilo.yml').content,
+    projectPath: folderPath,
+    palettes: loadPalettes(folderPath),
+    terminalStyle: loadTerminalStyle(folderPath),
+    syntax: loadAllSyntax(folderPath),
+    airlineStyles: loadAllStatusStyles(folderPath, 'airline'),
+    lightlineStyles: loadAllStatusStyles(folderPath, 'lightline')
+  }
+}
+
+function ymlsInFolder (folderPath: string, secondFolder?: string): string[] {
+  const finalPath = secondFolder
+    ? path.resolve(folderPath, secondFolder)
+    : folderPath
+  if (!fs.existsSync(finalPath)) {
+    throw new Error('folder doesn\'t exists: ' + finalPath)
+  }
+  return fs.readdirSync(finalPath, 'utf8')
+    .filter(filename => filename.endsWith('.yml'))
+}
+
+function loadPalettes (folderPath: string): Palettes {
+  const filepaths = ymlsInFolder(folderPath)
+  const palettes = {} as Palettes
+  filepaths.forEach(file => {
+    const palette = loadPalette(file)
+    palettes[palette.name] = palette
+  })
+  return palettes
+}
+
+function loadAllSyntax (folderPath: string): SyntaxFile[] {
+  const filepaths = ymlsInFolder(folderPath)
+  return filepaths.map(loadSyntax)
+}
+
+type StatusStyles = Record<string, StatusStyle>
+
+function loadAllStatusStyles (folderPath: string, kind: string): StatusStyles {
+  const filepaths = ymlsInFolder(folderPath, kind)
+  const statusStyle = {} as StatusStyles
+  filepaths.forEach(filepath => {
+    const style = loadStatusStyle(filepath)
+    statusStyle[style.name] = style
+  })
+  return statusStyle
+}
