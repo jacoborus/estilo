b9ff6a1daaecbcd3c5ba5381a715d76ccaa6f043
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-10 00:10:23 +0200

    Update: Use handlebars to render status lines

--- src/render-project.ts
+++ src/render-project.ts
@@ -1,27 +1,25 @@
 import { Project } from './common'
-
 import {
   writeScheme,
   writeStatus
 } from './writers'
 import { renderColorscheme } from './render-colorscheme'
-import { renderAirline } from './render-airline'
-import { renderLightline } from './render-lightline'
+import { renderStatus } from './render-status'
 
 export function renderProject (project: Project): void {
   const { config } = project
   config.colorschemes.forEach(config => {
-    const rendered = renderColorscheme(config, project)
+    const rendered = renderColorscheme(config, project, 'airline')
     writeScheme(rendered, config.name, project.folderPath)
   })
 
   config.airlines.forEach(config => {
-    const rendered = renderAirline(config, project)
+    const rendered = renderStatus(config, project, 'airline')
     writeStatus('airline', rendered, config.name, project.folderPath)
   })
 
   config.lightlines.forEach(config => {
-    const rendered = renderLightline(config, project)
+    const rendered = renderStatus(config, project, 'lightline')
     writeStatus('lightline', rendered, config.name, project.folderPath)
   })
 }
