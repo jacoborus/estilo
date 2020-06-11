ca7bbeec8672bf3edf9728713c45b97195d93644
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-11 14:36:33 +0200

    Update: Optimize select-syntax

--- src/cli/select-syntax.ts
+++ src/cli/select-syntax.ts
@@ -4,35 +4,31 @@ import fs from 'fs'
 import { installTemplates } from './install-templates'
 
 export function selectSyntax (projectPath: string) {
-  const syntaxFolder = path.resolve(__dirname, '../..', 'templates/syntax')
+  const syntaxFolder = path.resolve(__dirname, '../../templates/syntax')
   const syntaxDestFolder = path.resolve(projectPath, 'estilo/syntax')
   const templateFiles = fs.readdirSync(syntaxFolder)
   const installedSyntax = fs.readdirSync(syntaxDestFolder)
 
-  const templateChoices = templateFiles.map(f => {
-    const isDisabled = installedSyntax.indexOf(f) > -1
-    let tName = f.slice(0, -4)
+  const templateChoices = templateFiles.map(filename => {
+    const isDisabled = installedSyntax.includes(filename)
+    let syntaxName = filename.slice(0, -4)
     if (isDisabled) {
-      tName += ' (already installed)'
+      syntaxName += ' (installed)'
     }
-
     return {
-      name: tName,
-      value: f,
+      name: syntaxName,
+      value: filename,
       disabled: isDisabled
     }
   })
 
-  const questions = [
-    {
-      type: 'checkbox',
-      message: 'Select some extra syntax templates',
-      name: 'templates',
-      choices: templateChoices
-    }
-  ]
+  const questions = [{
+    type: 'checkbox',
+    message: 'Select some extra syntax templates',
+    name: 'templates',
+    choices: templateChoices
+  }]
 
-  inquirer.prompt(questions).then(function (answers) {
-    installTemplates(answers.templates as string[])
-  })
+  inquirer.prompt(questions)
+    .then(answers => installTemplates(answers.templates as string[]))
 }
