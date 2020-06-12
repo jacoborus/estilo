8bd83b89d0a19edc2c6c4423bc6f43f4af0af06a
Jacobo Tabernero <jacoborus@gmail.com>
2020-06-12 15:06:55 +0200

    Update: Better error output

--- src/crack.ts
+++
@@ -1,7 +0,0 @@
-export function crack (message: string, data?: Record<string, string>): never {
-  console.log('Error:')
-  console.log(message)
-  data && console.dir(data)
-  process.exit(1)
-}
-
---
+++ src/crash.ts
@@ -0,0 +1,15 @@
+import chalk from 'chalk'
+
+type ErrorOpts = Record<string, string>
+
+export function crack (message: string, data?: ErrorOpts): never {
+  console.log(chalk.red('Error: ' + message))
+  if (data) console.log(chalk.red(dataToText(data)))
+  process.exit(1)
+}
+
+function dataToText (data: ErrorOpts): string {
+  return Object.keys(data)
+    .map(key => `- ${key}: ${data[key]}`)
+    .join('\n')
+}
