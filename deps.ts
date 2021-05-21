0788daa3aebabcb34a58c1049de28e9f6cec8225
Jacobo Tabernero Rey <jacoborus@gmail.com>
2021-05-21 15:28:24 +0200

    Update: move version to a .ts file

--- deps.ts
+++ deps.ts
@@ -1,6 +1,11 @@
 import { resolve } from "https://deno.land/std/path/mod.ts";
 import { Handlebars } from "https://deno.land/x/handlebars/mod.ts";
 
+export * as version from "./version.ts";
+
+export { resolve, basename } from "https://deno.land/std/path/mod.ts";
+export { ensureDirSync, existsSync } from "https://deno.land/std/fs/mod.ts";
+
 export {
   Input,
   Checkbox,
@@ -10,23 +15,19 @@ export {
   Command,
   HelpCommand,
 } from "https://deno.land/x/cliffy/command/mod.ts";
+
 export {
   parse as yamlParse,
   parseAll as yamlParseAll,
   stringify as yamlStringify,
 } from "https://deno.land/std@0.97.0/encoding/yaml.ts";
-export { resolve, basename } from "https://deno.land/std/path/mod.ts";
-export { ensureDirSync, existsSync } from "https://deno.land/std/fs/mod.ts";
+
 export { green, yellow, red } from "https://deno.land/std/fmt/colors.ts";
 
 export { hexterm } from "https://raw.githubusercontent.com/jacoborus/hexterm/master/src/hexterm.ts";
 
-export const handlebars = new Handlebars();
-
 export const __dirname = new URL(".", import.meta.url).pathname;
-export const version = Deno.readTextFileSync(
-  resolve(__dirname, "version")
-).trim();
+export const handlebars = new Handlebars();
 
 export const mustaches = {
   colorscheme: resolve(__dirname, "mustaches/colorscheme.hbs"),
