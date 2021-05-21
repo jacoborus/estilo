f9c374712204c636481fb1da8f422dc30dd56dfb
Jacobo Tabernero Rey <jacoborus@gmail.com>
2021-05-21 16:03:01 +0200

    Update: render project asynchronous

--- src/render-project.ts
+++ src/render-project.ts
@@ -4,25 +4,26 @@ import { renderColorscheme } from "./render-colorscheme.ts";
 import { renderStatus } from "./render-status.ts";
 import { writeScheme, writeStatus } from "./writers.ts";
 
-export function renderProject(project: Project): void {
-  const { config } = project;
-  config.colorschemes.forEach(async (config) => {
+export async function renderProject(project: Project): Promise<void> {
+  const { config: projectConfig } = project;
+  for (const config of projectConfig.colorschemes) {
     const rendered = await renderColorscheme(config, project);
     writeScheme(rendered, config.name, project.folderPath);
-  });
+  }
 
-  if (config.airline) {
-    config.airline.forEach(async (config) => {
+  if (projectConfig.airline) {
+    for (const config of projectConfig.airline) {
       const rendered = await renderStatus(config, project, "airline");
       writeStatus("airline", rendered, config.name, project.folderPath);
-    });
+    }
   }
 
-  if (config.lightline) {
-    config.lightline.forEach(async (config) => {
+  if (projectConfig.lightline) {
+    for (const config of projectConfig.lightline) {
       const rendered = await renderStatus(config, project, "lightline");
       writeStatus("lightline", rendered, config.name, project.folderPath);
-    });
+    }
   }
+
   console.log(green("✓  Done, your theme is ready\n"));
 }
