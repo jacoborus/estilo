724bbaba925a5acc753075abdc4eade14cbe5824
Jacobo Tabernero Rey <jacoborus@gmail.com>
2025-02-27 04:09:38 +0100

    Update package.json info in build_npm.ts

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -25,18 +25,28 @@ await build({
   package: {
     name: "estilo",
     version: version as string,
-    description: "Create color schemes for Vim, Airline and Lightline",
+    description: "Create color schemes for Vim, NeoVim, Airline and Lightline",
     main: "esm/estilo.js",
     license: "MIT",
-    author: "Jacobo Tabernero Rey - http://jacobo.codes",
+    author: "Jacobo Tabernero Rey - https://jacobo.codes",
     homepage: "https://github.com/jacoborus/estilo",
-    keywords: ["vim", "color", "scheme", "schemes", "themes", "generator"],
+    keywords: [
+      "vim",
+      "color",
+      "scheme",
+      "schemes",
+      "themes",
+      "generator",
+      "neovim",
+      "lsp",
+      "treesitter",
+    ],
     repository: {
       type: "git",
-      url: "git@github.com:jacoborus/hexterm.git",
+      url: "git@github.com:jacoborus/estilo.git",
     },
     bugs: {
-      url: "https://github.com/jacoborus/hexterm/issues",
+      url: "https://github.com/jacoborus/estilo/issues",
     },
   },
 
